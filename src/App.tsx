a3189ad51f5f44ce14dd3d02386adbb72a912b89
vasvasvv <nesterenkovasil9@gmail.com>
2026-03-06 17:56:49 +0000

    121#$

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import ScrollToTop from "@/components/ScrollToTop";
 import Index from "@/pages/Index";
-import Implantation from "@/pages/Implantation";
+import Implantaciya from "@/pages/Implantaciya";
 import Protezuvannya from "@/pages/Protezuvannya";
 import LikuvannyaKariesu from "@/pages/LikuvannyaKariesu";
-import ProfessionalCleaning from "@/pages/ProfessionalCleaning";
-import EstetychnaStomat from "@/pages/EstetychnaStomat";
-import DiagnosticsPage from "@/pages/DiagnosticsPage";
+import ProfesijneOchischennya from "@/pages/ProfesijneOchischennya";
+import EstetychnaStomatologiya from "@/pages/EstetychnaStomatologiya";
+import DiagnostikaZubiv from "@/pages/DiagnostikaZubiv";
 import Contacts from "@/pages/Contacts";
 
 export default function App() {
@@ -17,12 +17,12 @@ export default function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/implantation" element={<Implantation />} />
+        <Route path="/implantaciya" element={<Implantaciya />} />
         <Route path="/protezuvannya" element={<Protezuvannya />} />
         <Route path="/likuvannya-kariesu" element={<LikuvannyaKariesu />} />
-        <Route path="/professional-cleaning" element={<ProfessionalCleaning />} />
-        <Route path="/estetychna-stomatolohiya" element={<EstetychnaStomat />} />
-        <Route path="/diagnostyka-renthen" element={<DiagnosticsPage />} />
+        <Route path="/profesijne-ochischennya" element={<ProfesijneOchischennya />} />
+        <Route path="/estetychna-stomatolohiya" element={<EstetychnaStomatologiya />} />
+        <Route path="/diagnostika-zubiv" element={<DiagnostikaZubiv />} />
         <Route path="/contacts" element={<Contacts />} />
       </Routes>
     </BrowserRouter>
